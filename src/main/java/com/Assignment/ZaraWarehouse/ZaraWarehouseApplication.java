8b1a17360b1dec683ad86b219cea3f124097c706
Mitul <mitulchampaneri@gmail.com>
2022-10-07 16:40:29 -0400

    Added SwaggerUI

--- src/main/java/com/Assignment/ZaraWarehouse/ZaraWarehouseApplication.java
+++ src/main/java/com/Assignment/ZaraWarehouse/ZaraWarehouseApplication.java
@@ -2,7 +2,12 @@ package com.Assignment.ZaraWarehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@EnableWebMvc
 @SpringBootApplication
 public class ZaraWarehouseApplication {
 
---
+++ src/main/java/com/Assignment/ZaraWarehouse/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.Assignment.ZaraWarehouse.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()  
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(apiInfo()).useDefaultResponseMessages(false);
+         }
+    
+    @Bean
+    public ApiInfo apiInfo() {
+        final ApiInfoBuilder builder = new ApiInfoBuilder();
+        builder.title("Warehouse API through Swagger UI").version("1.0").license("(C) Copyright Test")
+        .description("List of all the APIs of Warehouse API through Swagger UI");
+        return builder.build();
+        }
+    }
+
